db4e4d72e0c7147c29fa73efd93f3812d83cc148
mrlewismharris <lewis_h_97@hotmail.co.uk>
2021-07-15 22:24:14 +0100

    "instant preview" checbox
    Added an instant feedback checkbox which will enable the popper to show the typed word's search results (for first word function) within the server console textbox. Still need to actually implement the functionality (as with everything else) and fix a current bug where the types text doesn't appear in the console

--- client/open-newsroom/src/components/ServerConsole/ServerConsole.js
+++ client/open-newsroom/src/components/ServerConsole/ServerConsole.js
@@ -1,5 +1,5 @@
-import { AppBar, Button, Container, Dialog, FormControl, IconButton, Input, InputAdornment, Slide, TextField, Toolbar, Typography } from "@material-ui/core";
-import React, { useRef, useState } from "react";
+import { AppBar, Button, Checkbox, Container, Dialog, FormControlLabel, IconButton, Slide, TextField, Toolbar, Tooltip, Typography } from "@material-ui/core";
+import React, { useEffect, useRef, useState } from "react";
 import CloseIcon from '@material-ui/icons/Close';
 import './ServerConsole.css'
 
@@ -15,13 +15,34 @@ export default function ServerConsole(props) {
   const initialConsole = [
     `Open Newsroom Server Console v0.0.1`,
     ` `,
-    `Start typing with "!" to see available commands`
+    `Start typing or execute "help" to see available commands`,
+    ` `
   ]
   const [contents, setContents] = useState(initialConsole)
+  const [consoleCollection, setConsoleCollection] = useState([])
+  const [previewCommands, setPreviewCommands] = useState(true)
+
+  useEffect(() => {
+    setConsoleCollection(contents.map((item) => (<p className="terminalText">{item}</p>)))
+    setTimeout(() => {console.log(contents)}, 250)
+  }, [contents])
 
   function closeConsole() {
     setContents(initialConsole)
     props.onClose()
+  }
+
+  function sniffCommand(text) {
+    if (text[0] === "!") {
+      
+    }
+  }
+
+  function execute(target) {
+    let command = target.value
+    let updatedContents = contents
+    updatedContents.push(command)
+    setContents(updatedContents)
   }
 
   return (
@@ -44,6 +65,19 @@ export default function ServerConsole(props) {
             <CloseIcon />
           </IconButton>
           <Typography className="AppbarTitle" variant="h6">Server Console</Typography>
+          <Tooltip title="View available commands as you type">
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={previewCommands}
+                  onChange={() => {setPreviewCommands(!previewCommands)}}
+                  name="PreviewCommands"
+                  style={{color: "#fff"}}
+                />
+              }
+              label="Instant Preview"
+            />
+          </Tooltip>
           <Button
             //ref={anchorRef_File}
             variant="contained"
@@ -54,15 +88,8 @@ export default function ServerConsole(props) {
         </Toolbar>
       </AppBar>
 
-      <Container
-        fixed
-        className="consoleContainer"
-      >
-        {contents.map((item) => {
-          return(
-            <p className="terminalText">{item}</p>
-          )
-        })}
+      <Container fixed className="consoleContainer">
+        {consoleCollection}
       </Container>
       <Container
         fixed
@@ -74,6 +101,16 @@ export default function ServerConsole(props) {
           className="consoleInputText"
           fullWidth
           margin="normal"
+          onKeyDown={(e) => {
+            if (e.code === "Enter") {
+              execute(e.target)
+              // @ts-ignore
+              e.target.value=""
+            }
+          }}
+          onChange={(e) => {
+            sniffCommand(e.target.value)
+          }}
         />
       </Container>
     </Dialog>
